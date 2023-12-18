2b69a32a57ceeb09e49c668da7eab6af0c0bd251
frg <frg.aguitez@gmail.com>
2023-12-18 12:27:11 +0800

    patch 3.2
    ~ modified scoring system to be attached to character

--- Flario/src/flario/Character.java
+++ Flario/src/flario/Character.java
@@ -5,10 +5,12 @@ import javafx.scene.image.Image;
 
 public class Character extends Sprite {
 	private String name;
-	private boolean alive;
+	private boolean isAlive;
 	private int score;
+	private boolean isGrounded;
+	private int health;
 	
-	private final static Image CHARACTER_IMAGE = new Image("file:///C:/Users/ASUS/git/22-MINI-PROJ/Flario/src/images/character.png", 64, 64, false, false);
+	private final static Image CHARACTER_IMAGE = new Image("file:src/images/character.png", 64, 64, false, false);
 //	private final static Image CHARACTER_IMAGE = new Image("images/character.png", 64, 64, false, false);
 	public final static double INITIAL_X = 0;
 	public final static double INITIAL_Y = 410;
@@ -16,11 +18,16 @@ public class Character extends Sprite {
 	public final static double CHARACTER_SPEEDX = 3;
 	public final static double CHARACTER_WIDTH = 64;
 	public final static double CHARACTER_HEIGHT = 64;
-
+	public final static int INIT_SCORE = 50;
+	
 	Character(String name){
        	super(Character.INITIAL_X, Character.INITIAL_Y, Character.CHARACTER_WIDTH, Character.CHARACTER_HEIGHT, Character.CHARACTER_IMAGE);
 		this.name = name;
-		this.alive = true;
+		this.isAlive = true;
+		this.score = INIT_SCORE;
+		this.isGrounded = false;
+		this.health = 100;
+		
 	}
 
 	String getName(){
@@ -31,16 +38,30 @@ public class Character extends Sprite {
 		return this.score;
 	}
 
-    void die(){
-    	this.alive = false;
-    }
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public int getHealth() {
+		return this.health;
+	}
+	
+	public void setHealth(int health) {
+		this.health = health;
+		if(this.health <= 0) {
+			this.isAlive = false;
+		}
+	}
 
-    void gainScore(int increase){
-    	this.score+=increase;
-    	System.out.println("Score: "+score);
+    public void die(){
+    	this.isAlive = false;
     }
 
     boolean isAlive(){
-    	return this.alive;
+    	return this.isAlive;
+    }
+    
+    boolean isGrounded() {
+    	return this.isGrounded;
     } 
 }
