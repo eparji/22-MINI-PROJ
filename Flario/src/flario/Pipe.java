b34778b30c99264c5c27baf6870714559b4a34a4
frg <frg.aguitez@gmail.com>
2023-12-18 16:14:44 +0800

    dec 17 patch 5
    + added new png package (sprites)
    + imported item images
    ~ migrated sprite images
    ~ modified sources in new sprite declarations

--- Flario/src/flario/Pipe.java
+++ Flario/src/flario/Pipe.java
@@ -9,13 +9,13 @@ public class Pipe extends Sprite{
     private double height;
     private double width;
     
-    private final static Image PIPE_IMAGE = new Image("file:C:/Users/ASUS/git/22-MINI-PROJ/Flario/src/images/pipe.png");
+    private final static Image PIPE_IMAGE = new Image("file:src/sprites/pipe.png");
 //    private final static Image PIPE_IMAGE = new Image("images/pipe.png");
     private final static double PIPE_WIDTH = 128;
 
     public Pipe(double positionX, double positionY, boolean isFaceUp, int height) {
     	super(positionX, positionY, Pipe.PIPE_WIDTH, height, Pipe.PIPE_IMAGE);
-        //this.setSize(isFaceUp ? "/images/up_pipe.png" : "/images/down_pipe.png", 70, height);
+        //this.setSize(isFaceUp ? "/sprites/up_pipe.png" : "/sprites/down_pipe.png", 70, height);
         this.width = 70;
         this.height = height;
         this.locationX = 400;
