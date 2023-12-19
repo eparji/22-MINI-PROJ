a7483227707ebe9de168539bae29a0258aef644e
frg <frg.aguitez@gmail.com>
2023-12-19 11:33:08 +0800

    patch 1
    ~ small fix

--- Flario/src/flario/Pipe.java
+++ Flario/src/flario/Pipe.java
@@ -10,27 +10,26 @@ public class Pipe extends Sprite{
     private double height;
     private double width;
     
-    private final static Image PIPE_IMAGE = new Image("pipe.png");
-//=======
-//    private final static Image PIPE_IMAGE = new Image("file:src/sprites/pipe.png");
-//>>>>>>> branch 'master' of https://github.com/eparji/22-MINI-PROJ.git
+//    private final static Image PIPE_IMAGE = new Image("pipe.png");
+    
+    private final static Image PIPE_IMAGE = new Image("file:src/sprites/pipe.png");
 //    private final static Image PIPE_IMAGE = new Image("images/pipe.png");
     private final static double CHARACTER_SPEED = 1;
     private final static double PIPE_WIDTH = 100;
     private final static double PIPE_HEIGHT = 525;
     
-    private final static Image BOTTOM_PIPE_IMAGE = new Image(Pipe.class.getResource("new_pipe.png").toString(), PIPE_WIDTH, PIPE_HEIGHT, false, false);
-    private final static Image TOP_PIPE_IMAGE = new Image(Pipe.class.getResource("top_new_pipe.png").toString(), PIPE_WIDTH, PIPE_HEIGHT, false, false);
+    private final static Image BOTTOM_PIPE_IMAGE = new Image(Pipe.class.getResource("/sprites/new_pipe.png").toString(), PIPE_WIDTH, PIPE_HEIGHT, false, false);
+    private final static Image TOP_PIPE_IMAGE = new Image(Pipe.class.getResource("/sprites/top_new_pipe.png").toString(), PIPE_WIDTH, PIPE_HEIGHT, false, false);
     
     public Pipe(double positionX, double positionY, boolean isFaceUp) {
-//    	super(positionX, positionY, Pipe.PIPE_WIDTH, height, Pipe.PIPE_IMAGE);
-//        //this.setSize(isFaceUp ? "/sprites/up_pipe.png" : "/sprites/down_pipe.png", 70, height);
-//        this.width = 70;
-//        this.height = height;
+    	super(positionX, positionY, Pipe.PIPE_HEIGHT, Pipe.PIPE_WIDTH, Pipe.PIPE_IMAGE);
+//        this.setSize(isFaceUp ? "/sprites/up_pipe.png" : "/sprites/down_pipe.png", 70, height);
+        this.width = 70;
+        this.height = 125;
 //        this.locationX = 400;
 //        this.locationY = isFaceUp? 600 - height : 0;
-//        this.resizeImage(PIPE_IMAGE.getUrl(), PIPE_WIDTH, height);
-    	super(positionX, positionY, Pipe.PIPE_WIDTH, Pipe.PIPE_HEIGHT, isFaceUp? BOTTOM_PIPE_IMAGE: TOP_PIPE_IMAGE);
+        this.resizeImage(PIPE_IMAGE.getUrl(), PIPE_WIDTH, height);
+//    	super(positionX, positionY, Pipe.PIPE_WIDTH, Pipe.PIPE_HEIGHT, isFaceUp? BOTTOM_PIPE_IMAGE: TOP_PIPE_IMAGE);
 
     }
     
